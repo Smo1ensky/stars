7d437e6601d74bc819d9a54641d5b0f384e81a8e
RoyMonroe <roymonroeone@gmail.com>
2021-03-02 01:47:17 +0200

    3 stars

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import {Container, Square} from '../styles/style'
-import Star from "../components/Stars";
 import React, {useRef} from "react";
-import {Context} from '../context/Context'
+import Star from '../components/Stars'
 
+const reducer = (accumulator:number, currentValue:number) => accumulator + currentValue;
 function randomStars(min: number, max: number) {
     // случайное число от min до (max+1)
     let rand = min + Math.random() * (max + 1 - min);
@@ -16,34 +16,48 @@ function randomVolume(min: number, max: number, failOn: any) {
     return failOn.includes(num) ? randomVolume(min, max, failOn) : num;
 }
 
-export default function Home() {
+export default function Home({quantityStars}) {
+    console.log("server",quantityStars)
     const [score, setScore] = React.useState(0)
-    const [stars, setStars] = React.useState([1])
+    const [stars, setStars] = React.useState(quantityStars)
     const [loading, setLoading] = React.useState(false)
-
     const intervalRef = useRef();
 
     React.useLayoutEffect(() => {
-
-
         if (loading) {
+            let newQuantityStars = setTimeout(() => {
+                let result = stars.reduce(reducer)
+                setScore(result)
+                let quantityStars = Array.from(Array(randomStars(1, 3)), x => randomVolume(-5, 5, 0))
+                setStars(quantityStars)
+                console.log("1 quantityStars:",quantityStars, "1 result:",result,"1 score", score)
+            }, 4900)
 
-            const newQuantityStars = setInterval(() => {
-                const quantityStars = Array.from(Array(randomStars(1, 3)).keys())
-                setStars(quantityStars);
 
-            }, 5000);
-            intervalRef.current = newQuantityStars
 
         }
+
+
+    }, [loading])
+
+    React.useEffect(() => {
+        if (loading) {
+            let newQuantityStars = setTimeout(() => {
+                let result = stars.reduce(reducer)
+                setScore(score=>result+score)
+                let quantityStars = Array.from(Array(randomStars(1, 3)), x => randomVolume(-5, 5, 0))
+                setStars(quantityStars)
+                console.log("2 quantityStars:",quantityStars, "2 result:",result,"2 score", score)
+            }, 4950)
+            intervalRef.current = newQuantityStars
+        }
         return () => clearInterval(intervalRef.current);
-        console.log(stars)
-    }, [score])
+    }, [stars])
 
 
     return (
 
-        <Context.Provider value={[score, setScore, loading]}>
+        <>
             <Head>
                 <title>Stars</title>
                 <link rel="icon" href="/favicon.ico"/>
@@ -67,15 +81,14 @@ export default function Home() {
                 <Square>
                     {loading ? (
                         <>
-                            {stars.map((star: number) => (
-                                    <div style={{
-                                        display:"flow-root",
-                                        transform: `translateX(${randomStars(0, 150)}px)`,
-                                    }} key={star}>
-                                        <Star num={randomVolume(-5, 5, 0)}/>
-                                    </div>
-                                )
-                            )}
+                            {stars.map((star, index) => (
+                                <div style={{
+                                    display: "flow-root",
+                                    transform: `translateX(${randomStars(0, 150)}px)`,
+                                }} key={index}>
+                                    <Star num={star}/>
+                                </div>
+                            ))}
                         </>
                     ) : (
                         <div>Для старта нажмите кнопку "Запуск"...</div>
@@ -85,26 +98,28 @@ export default function Home() {
 
             </Container>
 
-        </Context.Provider>
+        </>
     )
 }
-// export async function getStaticProps() {
-//     function randomStars(min: number, max: number) {
-//         // случайное число от min до (max+1)
-//         let rand = min + Math.random() * (max + 1 - min);
-//         return Math.floor(rand);
-//     }
-//
-//     function randomVolume(min:number, max:number, failOn:array) {
-//         failOn = Array.isArray(failOn) ? failOn : [failOn]
-//         let num = Math.floor(Math.random() * (max - min + 1)) + min;
-//         return failOn.includes(num) ? generateRandom(min, max, failOn) : num;
-//     }
-//     let numb:number = randomVolume(-5,5,0)
-//     let quantityStars = Array.from(Array(randomStars(1, 3)).keys())
-//     return {
-//         props: {quantityStars, numb},
-//         revalidate: 5,
-//
-//     }
-// }
+
+export async function getStaticProps() {
+    function randomStars(min: number, max: number) {
+        // случайное число от min до (max+1)
+        let rand = min + Math.random() * (max + 1 - min);
+        return Math.floor(rand);
+    }
+
+    function randomVolume(min: number, max: number, failOn: any) {
+        failOn = Array.isArray(failOn) ? failOn : [failOn]
+        let num = Math.floor(Math.random() * (max - min + 1)) + min;
+        return failOn.includes(num) ? randomVolume(min, max, failOn) : num;
+    }
+
+    const quantityStars = Array.from(Array(randomStars(1, 3)), x => randomVolume(-5, 5, 0))
+
+    return {
+        props: {quantityStars},
+
+
+    }
+}
